c8564e7772191f73b99f1529f8a0a74c37b70ae2
ISLYLGM <isabely.mts2000@gmail.com>
2025-04-24 09:31:18 -0300

    cadastrarProduto.java

--- src/main/cadastrarProduto.java
+++ src/main/cadastrarProduto.java
@@ -37,7 +37,7 @@ public class cadastrarProduto {
         int quantidade = inputCadastro.nextInt();
 
         Produto novoProduto = new Produto(nome, preco, quantidade);
-        listaProdutos.add(novoProduto);
+        listaProdutos.add(novoProduto);// Composição: Adiciona o objeto Produto à lista.
 
         System.out.println("Produto cadastrado com sucesso!");
         menu();
