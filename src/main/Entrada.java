e0408b625bd46d74d53675671eb0f45ac78ba814
ISLYLGM <isabely.mts2000@gmail.com>
2025-04-24 17:07:12 -0300

    Entrada.java

--- src/main/Entrada.java
+++ src/main/Entrada.java
@@ -6,20 +6,20 @@ import modelo.Produto;
 import static main.Mercado.menu;
 
 public class Entrada extends OperacaoEstoque {
-//Polimorfismo
+//Polimorfismo (sobrescrita do método abstrato executar() )
     @Override
     public void executar() {
         System.out.println("Informe o ID do produto para entrada de estoque:");
         int id = input.nextInt();
 
-        Produto produto = buscarProdutoPorId(id);
+        Produto produto = buscarProdutoPorId(id);// Busca o produto na lista (composição)
 
         if (produto != null) {
             System.out.println("Informe a quantidade a ser adicionada:");
             int qtd = input.nextInt();
 
             if (qtd > 0) {
-                produto.setQuantidade(produto.getQuantidade() + qtd);
+                produto.setQuantidade(produto.getQuantidade() + qtd);// Atualiza quantidade
                 System.out.println("Estoque atualizado com sucesso!");
             } else {
                 System.out.println("Quantidade inválida.");
